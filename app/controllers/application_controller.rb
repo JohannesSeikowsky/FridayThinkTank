74d26d5c6396f93bf4f99cb59482c92f44911755
Johannes Seikowsky <seikowsky@googlemail.com>
2015-01-11 22:34:42 +0100

    adding name attr to user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,30 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # Part 1: Addin a name attribute to the Devise User model
+  before_action :configure_devise_permitted_parameters, if: :devise_controller?
+
+
   private
-  # Overwriting the sign_out redirect path method
+  # Overwriting the sign_out redirect path method / Devise
   def after_sign_out_path_for(user)
     root_path
+  end
+
+  # Part 2: Adding a name attribute to the Devise User model
+  protected
+  def configure_devise_permitted_parameters
+    registration_params = [:name, :email, :password, :password_confirmation]
+
+    if params[:action] == 'update'
+      devise_parameter_sanitizer.for(:account_update) { 
+        |u| u.permit(registration_params << :current_password)
+      }
+    elsif params[:action] == 'create'
+      devise_parameter_sanitizer.for(:sign_up) { 
+        |u| u.permit(registration_params) 
+      }
+    end
   end
 
 end
