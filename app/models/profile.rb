46a05d375117e0106eca88f913db8bf15e972982
Johannes Seikowsky <seikowsky@googlemail.com>
2015-01-13 03:27:06 +0100

    push

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,7 +1,8 @@
 class Profile < ActiveRecord::Base
   # The Paperclip Image attachment
-  has_attached_file :image, styles: { medium: "320x240"}
+  has_attached_file :image, styles: { medium: "320x240" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  validates_attachment :image, presence: true
 
   belongs_to :user
 end
