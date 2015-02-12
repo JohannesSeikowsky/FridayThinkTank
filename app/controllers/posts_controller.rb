d0033407fc9a255f529ea1f2258fa8710bd7ab14
Johannes Seikowsky <seikowsky@googlemail.com>
2015-02-12 22:33:14 +0000

    psh

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,7 +20,7 @@ class PostsController < ApplicationController
     @user = @post.user
     @profile = @user.profile
     @new_comment = @post.comments.build
-    @posts = Post.last(4)
+    @posts = Post.last(5)
   end
 
   # GET /posts/new
