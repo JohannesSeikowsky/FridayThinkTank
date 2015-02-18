67b5e3335545cc21e558906d2bb4a1007a3a0e90
Johannes Seikowsky <seikowsky@googlemail.com>
2015-02-18 20:17:26 +0000

    paperclip fix?

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,7 +2,7 @@ class Profile < ActiveRecord::Base
   # The Paperclip Image attachment
   has_attached_file :image, styles: { medium: "320x240" }
 
-  validates_attachment content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] }
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   belongs_to :user
 end
