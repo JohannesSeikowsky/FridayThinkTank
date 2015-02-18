4e95fd2a631ce4192abb0c1ba84d6580fd6e4209
Johannes Seikowsky <seikowsky@googlemail.com>
2015-02-18 20:05:55 +0000

    adding paperclip attachment content validation

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,5 +2,7 @@ class Profile < ActiveRecord::Base
   # The Paperclip Image attachment
   has_attached_file :image, styles: { medium: "320x240" }
 
+  validates_attachment content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] }
+
   belongs_to :user
 end
